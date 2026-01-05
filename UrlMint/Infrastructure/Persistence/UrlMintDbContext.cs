760f3340100bb74465aeb02bf1efeab6fd0284f6
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-06 01:32:00 +0300

    Add postgreSQL integration with EF Core

---
+++ UrlMint/Infrastructure/Persistence/UrlMintDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlMint.Domain.Entities;
+
+namespace UrlMint.Infrastructure.Persistence
+{
+    public class UrlMintDbContext : DbContext
+    {
+        public UrlMintDbContext(DbContextOptions<UrlMintDbContext> options) : base(options) { }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortUrl>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x=>x.LongUrl).IsRequired();
+
+                entity.Property(x => x.CreatedAt).HasDefaultValueSql("NOW()");
+            });
+
+
+        }
+
+    }
+}
--- UrlMint/Program.cs
+++ UrlMint/Program.cs
@@ -1,18 +1,30 @@
+using Microsoft.EntityFrameworkCore;
 using UrlMint.Domain.Interfaces;
 using UrlMint.Infrastructure.Encoding;
+using UrlMint.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Database
+builder.Services.AddDbContext<UrlMintDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        );
+});
 
+//DI Registration
+builder.Services.AddSingleton<IUrlEncoder, Base62Encoder>();
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-//DI Registration
-builder.Services.AddSingleton<IUrlEncoder,Base62Encoder>();
+
 
 
 
