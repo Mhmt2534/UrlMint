f8ec8783278d7386d170558ea7eb3ee305a99bac
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-08 01:39:18 +0300

    add click count to ShortUrl entity

--- UrlMint/Domain/Entities/ShortUrl.cs
+++ UrlMint/Domain/Entities/ShortUrl.cs
@@ -5,5 +5,6 @@
         public long Id { get; set; }
         public string LongUrl { get; set; }
         public DateTime CreatedAt { get; set; }
+        public long ClickCount { get; set; }
     }
 }
