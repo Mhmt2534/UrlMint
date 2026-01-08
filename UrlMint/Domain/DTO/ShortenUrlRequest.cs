13adb46a33d7ae2ce8318c1c17408b60273311ac
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-09 01:42:21 +0300

    add ShortUrl controller ,repository and apsettings template

---
+++ UrlMint/Domain/DTO/ShortenUrlRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlMint.Domain.DTO
+{
+    public class ShortenUrlRequest
+    {
+        public string LongUrl { get; set; } =string.Empty;  
+    }
+}
--- UrlMint/Program.cs
+++ UrlMint/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using UrlMint.Domain.Interfaces;
 using UrlMint.Infrastructure.Encoding;
 using UrlMint.Infrastructure.Persistence;
+using UrlMint.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<UrlMintDbContext>(options =>
 
 //DI Registration
 builder.Services.AddSingleton<IUrlEncoder, Base62Encoder>();
+builder.Services.AddScoped<IShortUrlRepository,ShortUrlRepository>();
 
 
 // Add services to the container.
