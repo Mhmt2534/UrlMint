2d1f22cb31936e99e6e5418cf90e9a4a53593bdb
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-20 16:52:37 +0300

    chore(db): apply click count migration

--- UrlMint/Infrastructure/Persistence/Migrations/20260119123809_UpdateClickCount.Designer.cs
+++ UrlMint/Infrastructure/Persistence/Migrations/20260119123809_UpdateClickCount.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UrlMint.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using UrlMint.Infrastructure.Persistence;
 namespace UrlMint.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(UrlMintDbContext))]
-    partial class UrlMintDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260119123809_UpdateClickCount")]
+    partial class UpdateClickCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UrlMint/Infrastructure/Persistence/Migrations/20260119123809_UpdateClickCount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlMint.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClickCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ClickCount",
+                table: "ShortUrls",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ClickCount",
+                table: "ShortUrls",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
