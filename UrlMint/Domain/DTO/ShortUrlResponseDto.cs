4b34058b7e92ac151e94f8997fe2c6325d094463
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-20 15:58:47 +0300

    feat: extend ShortUrl entity and response dto

--- UrlMint/Domain/DTO/ShortUrlResponseDto.cs
+++ UrlMint/Domain/DTO/ShortUrlResponseDto.cs
@@ -2,6 +2,7 @@
 {
     public class ShortUrlResponseDto
     {
+        public string ShortCode { get; set; }
         public string LongUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public int ClickCount { get; set; }
