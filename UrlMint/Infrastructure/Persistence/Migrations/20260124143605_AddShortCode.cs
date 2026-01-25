2276dae5a91ea78cedf89f741817c941f3bd8950
Mhmt2534 <mahmutgn540@gmail.com>
2026-01-25 16:43:45 +0300

    push migrates

---
+++ UrlMint/Infrastructure/Persistence/Migrations/20260124143605_AddShortCode.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlMint.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShortCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortCode",
+                table: "ShortUrls",
+                type: "character varying(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortCode",
+                table: "ShortUrls");
+        }
+    }
+}
