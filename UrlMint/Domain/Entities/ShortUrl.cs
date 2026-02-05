c4dac0ef177dbff16c17f5a37bf64dc3271cbb4a
Mhmt2534 <mahmutgn540@gmail.com>
2026-02-05 16:10:46 +0300

    chore(db): add ExpiresAt

--- UrlMint/Domain/Entities/ShortUrl.cs
+++ UrlMint/Domain/Entities/ShortUrl.cs
@@ -7,5 +7,9 @@
         public string? ShortCode { get; set; }
         public DateTime CreatedAt { get; set; }
         public int ClickCount { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+
+        public bool IsExpired => 
+            ExpiresAt.HasValue && ExpiresAt.Value < DateTime.UtcNow;
     }
 }
---
+++ UrlMint/Infrastructure/Persistence/Migrations/20260205130220_AddExpireDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlMint.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpireDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
