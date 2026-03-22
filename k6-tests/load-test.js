37f604bc30fd579da40def845dd1166c97793f13
Mhmt2534 <mahmutgn540@gmail.com>
2026-03-22 23:04:16 +0300

    localhost:8080

--- k6-tests/load-test.js
+++ k6-tests/load-test.js
@@ -11,7 +11,7 @@ export const options = {
   }
 };
 
-const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
+const BASE_URL = __ENV.BASE_URL || 'http://urlmint-api:8080';
 
 export function setup() {
   console.log("API'nin hazır olması bekleniyor (10sn)...");
